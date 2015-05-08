6e921416e7ae2b705c7e547bef464080cf20262a
Diego Dukão <diegodukao@gmail.com>
2015-05-08 15:03:21 -0300

    group controller and service. first version

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,9 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('GroupsCtrl', function($scope) {})
+.controller('GroupsCtrl', function($scope, Friends) {
+    $scope.friends = Friends.all();
+})
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
--- www/js/services.js
+++ www/js/services.js
@@ -1,2 +1,21 @@
 angular.module('starter.services', [])
 
+.factory('Friends', function() {
+    var friends = [{
+      id: 0,
+      name: "Fulano de Tal"
+    },{
+      id: 1,
+      name: "Beltrano da Silva"
+    },{
+      id: 2,
+      name: "Larisso"
+    }];
+    
+    return {
+      all: function() {
+        return friends;
+      }
+    };
+});
+
